ffb5854bdff14200bcd72c8834cd75006f8b1bdd
Tamás K. Papp <tkpapp@gmail.com>
2023-10-02 11:59:16 +0200

    add constraints to solver interface
    also build on JuliaSmoothOptimizer solvers

--- src/IdentificationDebugger.jl
+++ src/IdentificationDebugger.jl
@@ -3,12 +3,16 @@ Placeholder for a short summary about IdentificationDebugger.
 """
 module IdentificationDebugger
 
-export parameter, identification_problem, check_identification
+export parameter, endogeneous_parameter, identification_problem, check_identification
 
 using ArgCheck: @argcheck
 using DocStringExtensions: SIGNATURES
 using FillArrays: Fill
+using ADNLPModels: ADNLSModel
+using JSOSolvers: tron
 using Logging: @error
+using Percival: percival
+using ObjConsNLPModels: objcons_nlpmodel
 using TransformVariables: dimension, transform, inverse, as, asℝ
 using Random: default_rng
 using Statistics: mean
@@ -22,6 +26,7 @@ Base.@kwdef struct Parameter{TK,TT,TL,TU}
     transformation::TT
     lower_bound::TL
     upper_bound::TU
+    is_endogeneous::Bool
 end
 
 _default_transformation(a::Real) = asℝ
@@ -51,7 +56,20 @@ function parameter(known_value;
     @argcheck dimension(transformation) == length(lower_bound) == length(upper_bound)
     x = inverse(transformation, known_value)
     @argcheck all(lower_bound .≤ x .≤ upper_bound)
-    Parameter(; known_value, transformation, lower_bound, upper_bound)
+    Parameter(; known_value, transformation, lower_bound, upper_bound,
+              is_endogeneous = false)
+end
+
+"""
+$(SIGNATURES)
+
+An endogeneous parameter. Will be solved for when the problem is created.
+"""
+function endogeneous_parameter(transformation;
+                               lower_bound::AbstractVector = _default_lower_bound(transformation),
+                               upper_bound::AbstractVector = _default_upper_bound(transformation))
+    Parameter(; known_value = fill(NaN, dimension(transformation)),
+              transformation, lower_bound, upper_bound, is_endogeneous = true)
 end
 
 ###
@@ -81,11 +99,21 @@ mean_abs2(x::AbstractVector, y::AbstractVector) = mean(((x, y),) -> abs2(x - y),
 Base.@kwdef struct IdentificationProblem{TP<:NamedTuple,TM,TO,TN,}
     parameters::TP
     moments_and_constraint_calculator::TM
-    objective::TO
-    solution_norm::TN
-    solution_tol::Float64
-    random_x0_count::Int
-    minimum_convergence_ratio::Float64
+    constraint_dimension::Int = 0
+    objective::TO = nothing
+    solution_norm::TN = mean_abs2
+    solution_tol::Float64 = 1e-4
+    random_x0_count::Int = 10
+    minimum_convergence_ratio::Float64 = 1.0
+end
+
+function classify_parameters(problem::IdentificationProblem)
+    endogeneous = Symbol[]
+    exogeneous = Symbol[]
+    for (name, parameter) in pairs(problem.parameters)
+        push!(parameter.is_endogeneous ? endogeneous : exogeneous, name)
+    end
+    (endogeneous = Tuple(endogeneous), exogeneous = Tuple(exogeneous))
 end
 
 function free_parameters(problem::IdentificationProblem, free_variables::Val{S}) where S
@@ -112,18 +140,51 @@ function known_values(problem::IdentificationProblem, free_variables::Val{S}) wh
     map(p -> p.known_value, Base.structdiff(problem.parameters, NamedTuple{S}))
 end
 
+"""
+$(SIGNATURES)
+"""
+function solve_endogeneous_parameters!(problem::IdentificationProblem,
+                                      endogeneous_variables::Val{S}) where S
+    isempty(S) && return problem
+    (; moments_and_constraint_calculator, parameters) = problem
+    N = free_dimension(problem, endogeneous_variables)
+    transformation = free_transformation(problem, endogeneous_variables)
+    lb = free_lower_bound(problem, endogeneous_variables)
+    ub = free_upper_bound(problem, endogeneous_variables)
+    κ = known_values(problem, endogeneous_variables)
+    function F(x)
+        θ = transform(transformation, x)
+        moments_and_constraint_calculator(merge(κ, θ))[2]
+    end
+    x0 = zeros(N)
+    PF = ADNLSModel(F, x0, length(F(x0)), lb, ub)
+    stats = tron(PF)
+    @argcheck stats.solution_reliable
+    θ = transform(transformation, stats.solution)
+    for (k, v) in pairs(θ)
+        getproperty(parameters, k).known_value .= v
+    end
+    nothing
+end
 
 "$(SIGNATURES)"
 function identification_problem(moments_and_constraint_calculator,
                                 parameters::NamedTuple,
                                 solution_norm = mean_abs2, solution_tol = 1e-4,
                                 random_x0_count = 10, minimum_convergence_ratio = 1.0)
-    moments, constraint = moments_and_constraint_calculator(map(p -> p.known_value, parameters))
+    parameters = deepcopy(parameters) # don't modify caller's variables
+    problem0 = IdentificationProblem(; parameters, moments_and_constraint_calculator)
+    solve_endogeneous_parameters!(problem0,
+                                  Val(classify_parameters(problem0).endogeneous))
+    moments, constraint = moments_and_constraint_calculator(map(p -> p.known_value,
+                                                                problem0.parameters))
+    @argcheck all(constraint .≤ 1e-3) # FIXME don't hardcode this, use solver's tolerance
     objective = LeastSquaresObjective(moments)
-    IdentificationProblem(; parameters, moments_and_constraint_calculator, objective, solution_norm,
-                          solution_tol, random_x0_count, minimum_convergence_ratio)
+    IdentificationProblem(; parameters, moments_and_constraint_calculator, objective,
+                          solution_norm, solution_tol, random_x0_count,
+                          minimum_convergence_ratio,
+                          constraint_dimension = length(constraint))
 end
-
 
 ###
 ### partial problem we pass to optimizers
@@ -153,13 +214,13 @@ function known_x(pp::PartialProblem{S}) where S
     inverse(transformation, map(p -> p.known_value, free_parameters(parent_problem, Val(S))))
 end
 
-function calculate_objective_and_constraint(pp::PartialProblem{S}, x::AbstractVector) where {S}
+function calculate_objcons(pp::PartialProblem{S}, x::AbstractVector) where {S}
     (; parent_problem, transformation) = pp
     _known = known_values(parent_problem, Val(S))
     _free = transform(transformation, x)
     (; objective, moments_and_constraint_calculator) = parent_problem
     moments, constraint =  moments_and_constraint_calculator(merge(_free, _known))
-    objective(moments), constraint
+    pushfirst!(copy(constraint), objective(moments))
 end
 
 ###
@@ -189,32 +250,24 @@ function random_starting_point(lower_bound::AbstractVector, upper_bound::Abstrac
     map(_r, lower_bound, upper_bound)
 end
 
-const SOLVER_DOCS = """
-The solver is a function which will be called as
-
-```julia
-(; converged, x) = solver(f, x0, lb, ub, lc, uc)
-```
-where `converged` should be a boolean indicating convergence and `x` is the solution.
-"""
-
-function solve_and_status(pp::PartialProblem, solver, x0;
+function solve_and_status(pp::PartialProblem, x0;
                           lb = lower_bound(pp), ub = upper_bound(pp),
                           catch_errors = true)
-
-    (; solution_norm, solution_tol) = pp.parent_problem
+    (; solution_norm, solution_tol, constraint_dimension) = pp.parent_problem
+    F = objcons_nlpmodel(Base.Fix1(calculate_objcons, pp); x0, lvar = lb, uvar = ub)
     x̃ = known_x(pp)
     try
-        (; converged, x) = solver(Base.Fix1(calculate_objective, pp), x0, lb, ub, lc, uc)
+        stats = percival(F)
+        x = stats.solution
         Δ = solution_norm(x̃, x)
-        status = if !converged
+        status = if !stats.solution_reliable
             :nonconvergence
         elseif Δ ≤ solution_tol
             :convergence_correct
         else
             :convergence_incorrect
         end
-        (; x0, status)
+        (; x, status)
     catch e
         if catch_errors
             @warn "error message" sprint(showerror, e)
@@ -225,12 +278,12 @@ function solve_and_status(pp::PartialProblem, solver, x0;
     end
 end
 
-function solve_and_check(pp::PartialProblem{S}, solver; catch_errors = true) where S
+function solve_and_check(pp::PartialProblem{S}; catch_errors = true) where S
     lb = lower_bound(pp)
     ub = upper_bound(pp)
     (; random_x0_count::Int, minimum_convergence_ratio::Float64) = pp.parent_problem
     # NOTE parallelize this step
-    s = [solve_and_status(pp, solver, random_starting_point(lb, ub); lb, ub, catch_errors)
+    s = [solve_and_status(pp, random_starting_point(lb, ub); lb, ub, catch_errors)
          for _ in 1:pp.parent_problem.random_x0_count]
     s_e = findfirst(s -> s.status ≡ :error, s)
     if s_e ≢ nothing
@@ -249,20 +302,17 @@ end
 $(SIGNATURES)
 
 Solve the given problem step by step, checking that the known parameters are identified.
-
-## Solver interface
-
-$(SOLVER_DOCS)
 """
-function check_identification(solver, problem::IdentificationProblem; catch_errors = true)
-    known_variables = keys(problem.parameters)
-    free_variables = ()
-    while !isempty(known_variables)
-        v, known_variables... = known_variables
-        free_variables = (free_variables..., v)
-        pp = partial_problem(problem, Val(free_variables)) # non-inferrable, but that's ok
-        @info "solving partial model" free_variables
-        ok = solve_and_check(pp, solver; catch_errors)
+function check_identification(problem::IdentificationProblem; catch_errors = true)
+    (; exogeneous, endogeneous) = classify_parameters(problem)
+    known_parameters = exogeneous
+    free_parameters = endogeneous
+    while !isempty(known_parameters)
+        v, known_parameters... = known_parameters
+        free_parameters = (free_parameters..., v)
+        pp = partial_problem(problem, Val(free_parameters)) # non-inferrable, but that's ok
+        @info "solving partial model" free_parameters
+        ok = solve_and_check(pp; catch_errors)
         if !ok
             error("solution failed when adding variable $(v)")
         end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,35 @@
 using IdentificationDebugger
-using Test
-using ADNLPModels, JSOSolvers
+using Test, TransformVariables
 
-using IdentificationDebugger: partial_problem, lower_bound, upper_bound, calculate_objective
+using IdentificationDebugger: partial_problem, lower_bound, upper_bound, calculate_objcons
 
-parameters = (a = parameter(2.0), b = parameter([1.0, 3.0]))
+parameters = (a = parameter(2.0), b = parameter([1.0, 3.0]),
+              c = endogeneous_parameter(as(Array, 3)))
 
 "Matrix we use for tests, all that matters is that it is not singular & not block diagonal."
 const M = [0.5 0.3 0.2;
            0.7 1.9 -5.0
            -0.2 0.5 1.4]
 
+const z = [0.1, 0.2, -0.9]
+
 IP = identification_problem(parameters) do θ
-    (; a, b) = θ
-    r = M * vcat(a, b)
-    (c = r[1:2], d = r[3])
+    (; a, b, c) = θ
+    m = M * vcat(a, b)
+    r = M * c .- z
+    (c = m[1:2], d = m[3]), r
 end
+
+@test maximum(abs, M * IP.parameters.c.known_value .- z) ≤ 1e-8
 
 PP = partial_problem(IP, Val((:a,)))
 
 @test lower_bound(PP) == [-Inf]
 @test upper_bound(PP) == [Inf]
-@test calculate_objective(PP, [parameters.a.known_value]) == 0
+@test maximum(abs, calculate_objcons(PP, [parameters.a.known_value])) ≤ 1e-8
 
-function solver_ADNLP(objective, lb, ub, x0)
-    model = ADNLPModel(objective, x0, lb, ub)
-    stats = tron(model)
-    converged = stats.solution_reliable
-    x = stats.solution
-    (; x, converged)
-end
 
-@test check_identification(solver_ADNLP, IP)
+@test check_identification(IP)
 
 
 ## NOTE add JET to the test environment, then uncomment
